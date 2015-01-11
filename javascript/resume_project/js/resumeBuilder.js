18a32ce38930933454c05d761dc728035ce1e217
Harry Liu <yucheng.infosec@gmail.com>
2015-01-10 19:46:29 -0500

    MOOC: javascript course update

---
+++ javascript/resume_project/js/resumeBuilder.js
@@ -0,0 +1,35 @@
+//var formattedName = HTMLheaderName.replace("%data%", "Yucheng");
+//var role = "Hacker";
+//var formattedRole = HTMLheaderRole.replace("%data%", role);
+//
+//$("#header").prepend(role);
+//$("#header").prepend(formattedName);
+//
+
+var bio = {
+    "name": "Yucheng",
+    "role": "Hacker",
+    "Contact": "888-888-8888",
+    "skills": ["Python", "Web", "Testing"]
+};
+
+var education = {
+    "schools": [
+        {
+            "name": "GaTech",
+            "city": "Atlanta",
+            "degree": "MS",
+            "major": ["CS", "InfoSec"],
+            "dates": 2014,
+            "url": "http://www.gatech.edu"
+        },
+        {
+            "name": "Southeastern",
+            "city": "Nanjing",
+            "degree": "MS",
+            "major": ["InfoSec"],
+            "dates": 2012,
+            "url": "http://www.seu.edu"
+        }
+    ]
+};
